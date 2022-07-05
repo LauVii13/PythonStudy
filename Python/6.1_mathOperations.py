7c4d4e43ae607431eb28818faf1114ab765f49cc
LauVii13 <viniciusglaureano@gmail.com>
2022-07-05 19:19:18 -0300

    Basic operators and shorter forms

---
+++ Python/6.1_mathOperations.py
@@ -0,0 +1,27 @@
+from cgi import print_form
+
+
+print(10 + 3) # 13
+print(10 - 3) # 7
+print(10 * 3) #returns the result of multiplication -> 30
+print(10 / 3) #returns the result of division -> 3.333333...
+print(10 // 3) #returns a integer number -> 3
+print(10 % 3) #returns the rest of the division -> 1
+print(10 ** 3) #represents the exponent, = 10^3 -> 1000
+
+
+#=====================================================================================================================================================================#
+x = 10
+x = x + 3
+#Augmented assignment operator -> way to write the same code but in a shorter form
+x += 3 # x = x + 3
+x -= 3 # x = x - 3
+x *= 3 # x = x * 3
+x /= 3 # x = x / 3
+x //= 3 # x = x // 3
+x %= 3 # x = x % 3
+x **= 3 # x = x ** 3
+
+
+
+
