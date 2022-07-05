5dfb4e4400c50ae3904ea47fbedfd9015d6ed6c7
LauVii13 <viniciusglaureano@gmail.com>
2022-07-05 20:53:24 -0300

    importing math functions and reading the math module

---
+++ Python/6.3_mathFunctions.py
@@ -0,0 +1,10 @@
+import math # allows us to access different math functions
+
+x = 2.9
+round(x) # rounds a float number to an integer
+abs(x) # returns the absolute value, so, if x = -3, it'll return 3
+
+math.sqrt(4) # returns the square root of the number
+
+#If you need to discover other functions, just search "python 3 math module" in your browser, or use this link: https://docs.python.org/3/library/math.html
+#                                                                                            Portuguese-Brazil: https://docs.python.org/pt-br/3/library/math.html
