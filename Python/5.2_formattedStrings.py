4a0fbd4b3407445812e44f06fabf18e4a366d1c2
LauVii13 <viniciusglaureano@gmail.com>
2022-07-05 17:32:07 -0300

    Formatted Strings

---
+++ Python/5.2_formattedStrings.py
@@ -0,0 +1,9 @@
+first = 'Jhon'
+last = 'Smith'
+
+#we want to print the phrase: Jhon [Smith] is a coder
+message = first + ' [' + last + '] is a coder'
+
+#formatted string -> without the + to concatenate, it is a way of simplifying the last example
+msg = f'{first} [{last}] is a coder'
+print(msg)
