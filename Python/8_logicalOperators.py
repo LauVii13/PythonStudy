bea18337fcf30e2572c8cd6c0bdc8c7f545302a9
LauVii13 <viniciusglaureano@gmail.com>
2022-07-05 23:27:41 -0300

    AND, OR and NOT

---
+++ Python/8_logicalOperators.py
@@ -0,0 +1,31 @@
+#if applicant has high income AND good credit
+#   Eligible for loan
+
+has_high_income = True
+has_good_credit = True
+
+if has_high_income and has_good_credit:
+    print("Eligible for loan")
+    
+
+# LOGICAL OPERATORS
+# ---------------------------------
+# AND -> both conditions should be true to return true
+# OR -> at least one condition should be true to return true
+# NOT -> iverses any boolean value; how to use? --> not variable (for example, not has_high_income)
+# ---------------------------------
+# IF - - - - - - - - -> RETURNS
+# ---------------------------------
+# if True AND True -> True
+# if True AND False -> False
+# if False AND True -> False
+# if False AND False -> False
+# ---------------------------------
+# if True OR True -> True
+# if True OR False -> True
+# if False OR True -> True
+# if False OR False -> False
+# ---------------------------------
+# if NOT True -> False
+# if NOT False -> True
+# ---------------------------------
