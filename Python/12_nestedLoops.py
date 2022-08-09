733fce2ab9a505864585a83a8fc79af05563e339
LauVii13 <viniciusglaureano@gmail.com>
2022-08-08 22:00:38 -0300

    multiplication table challenge

--- Python/12_nestedLoops.py
+++ Python/12_nestedLoops.py
@@ -13,3 +13,13 @@ for x in range(4): # outer loop
     for y in range(3): # insider loop
         print(f'({x}, {y})')
         
+# =====================================================================================================================================================================#
+
+# Challenge
+# Do the multiplication table from 1 to 10 using nested loops
+# a x b = c
+
+for a in range(1, 11):
+    for b in range (1, 11):
+        print(f'{a} x {b} = {a * b}')
+    print('')
