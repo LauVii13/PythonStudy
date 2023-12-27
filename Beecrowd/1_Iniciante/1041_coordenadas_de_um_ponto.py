ac88209ae0c394b0f1dd3eb2a1446ae9f8126a03
LauVii13 <viniciusglaureano@gmail.com>
2023-12-27 20:39:28 -0300

    1_1041 finished

---
+++ Beecrowd/1_Iniciante/1041_coordenadas_de_um_ponto.py
@@ -0,0 +1,17 @@
+x, y = [float(e) for e in input().split()]
+
+if x == 0 and y == 0:
+    print("Origem")
+elif x == 0:
+    print("Eixo Y")
+elif y == 0:
+    print("Eixo X")
+else:
+    if x > 0 and y > 0:
+        print("Q1")
+    elif x < 0 and y > 0:
+        print("Q2")
+    elif x < 0 and y < 0:
+        print("Q3")
+    else:
+        print("Q4")
