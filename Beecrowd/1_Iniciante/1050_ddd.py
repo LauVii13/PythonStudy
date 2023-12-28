680accd6bd5cc88d21b502ecd2ba3d35f2d3ed27
LauVii13 <viniciusglaureano@gmail.com>
2023-12-28 00:35:26 -0300

    1_1050 finished (match doesn't works in python 3.9)

---
+++ Beecrowd/1_Iniciante/1050_ddd.py
@@ -0,0 +1,23 @@
+ddd = input()
+result = ''
+
+if ddd == '61':
+    result = 'Brasilia'
+elif ddd == '71':
+    result = 'Salvador'
+elif ddd == '11':
+    result = 'Sao Paulo'
+elif ddd == '21':
+    result = 'Rio de Janeiro'
+elif ddd == '32':
+    result = 'Juiz de Fora'
+elif ddd ==  '19':
+    result = 'Campinas'
+elif ddd == '27':
+    result = 'Vitoria'
+elif ddd == '31':
+    result = 'Belo Horizonte'
+else:
+    result = 'DDD nao cadastrado'
+        
+print(result)
