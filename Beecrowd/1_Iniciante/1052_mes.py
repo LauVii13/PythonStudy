960a25b434bb3fccdedd31a65d1c0ed260564dfd
LauVii13 <viniciusglaureano@gmail.com>
2023-12-28 00:55:14 -0300

    1_1052 finished

---
+++ Beecrowd/1_Iniciante/1052_mes.py
@@ -0,0 +1,5 @@
+month = int(input())
+
+list_months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+print(list_months[month - 1])
