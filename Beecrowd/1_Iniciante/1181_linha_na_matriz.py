d3403e0cfe636b837eda7770641d5d421468146f
LauVii13 <viniciusglaureano@gmail.com>
2024-01-01 15:04:21 -0300

    1_1181 finished

---
+++ Beecrowd/1_Iniciante/1181_linha_na_matriz.py
@@ -0,0 +1,17 @@
+lin = int(input())
+matriz = [[0 for i in range(12)] for j in range(12)]
+
+op = input()
+
+for i in range(12):
+    for j in range(12):
+        matriz[i][j] = float(input())
+
+soma = 0
+for e in matriz[lin]:
+    soma += e
+
+if op == "S":
+    print(f"{soma:.1f}")
+else:
+    print(f"{(soma / 12):.1f}")
