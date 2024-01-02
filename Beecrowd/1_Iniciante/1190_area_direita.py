190b0df55fc42af8f84c2bcf8684c71b811b9e71
LauVii13 <viniciusglaureano@gmail.com>
2024-01-02 10:55:01 -0300

    1_1190 finished

---
+++ Beecrowd/1_Iniciante/1190_area_direita.py
@@ -0,0 +1,20 @@
+matriz = [[0 for i in range(12)] for j in range(12)]
+
+op = input()
+
+for i in range(12):
+    for j in range(12):
+        matriz[i][j] = float(input())
+
+soma = 0
+cont = 0
+for i in range(12):
+    for j in range(12):
+        if j + i > 11 and j > i:
+            soma += matriz[i][j]
+            cont += 1
+
+if op == "S":
+    print(f"{soma:.1f}")
+else:
+    print(f"{(soma / cont):.1f}")
