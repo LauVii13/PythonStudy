b3d7b79c472d570cc60091e3562e9f7bbd5a930a
LauVii13 <viniciusglaureano@gmail.com>
2024-01-02 10:41:36 -0300

    1_1183 finished

---
+++ Beecrowd/1_Iniciante/1183_acima_da_diagonal_principal.py
@@ -0,0 +1,22 @@
+matriz = [[0 for i in range(12)] for j in range(12)]
+
+op = input()
+
+for i in range(12):
+    for j in range(12):
+        matriz[i][j] = float(input())
+
+
+
+soma = 0
+cont = 0
+for i in range(12):
+    for j in range(12):
+        if j > i:
+            soma += matriz[i][j]
+            cont += 1
+
+if op == "S":
+    print(f"{soma:.1f}")
+else:
+    print(f"{(soma / cont):.1f}")
