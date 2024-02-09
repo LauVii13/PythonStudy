56df0a381fd2d0e66d9b8021d36161dc6de124ae
LauVii <viniciusglaureano@gmail.com>
2024-02-09 16:45:54 -0300

    question g finished

--- Questions/VJudge/AA_Iniciante/Lista1/questao_g.py
+++ Questions/VJudge/AA_Iniciante/Lista1/questao_g.py
@@ -1,53 +1,19 @@
 t = int(input())
 for _ in range(t):
     n, x = map(int, input().split())
+
     lista = list(map(int, input().split()))
+    aux = list(map(lambda x: 1, lista))
 
-    extra = []
-    j = 0
-    fim_da_lista = True
-
-    for v in lista:
-        if v % x != 0:
-            fim_da_lista = False
+    i = 0
+    while True:
+        if lista[i] % x != 0:
             break
+        lista.append(lista[i] // x)
+        aux.append(aux[i] * x)
 
-        cont = 0
-        cpv = v
-        while cpv % x == 0:
-            cont += 1
-            cpv //= x
+        i += 1
 
-        extra.append(cont)
+    soma = sum(lista[i] * aux[i] for i in range(len(lista)))
 
-    if fim_da_lista:
-
-        indice_menor = extra.index(min(extra))
-        print(indice_menor)
-        segundo_menor = (
-            extra.index(min(extra[:indice_menor]))
-            if len(extra) > 2 or indice_menor <= 0
-            else indice_menor
-        )
-
-        for i in range(len(extra)):
-            if i < indice_menor:
-                extra[i] = (
-                    extra[indice_menor] + 1
-                    if extra[indice_menor] < extra[i]
-                    else extra[indice_menor]
-                )
-            else:
-                extra[i] = extra[indice_menor]
-
-        print(extra)
-
-        # while True:
-        #     if extra[j] % x != 0:
-        #         break
-        #     extra.append([extra[j][0] // x, extra[j][1] * x])
-        #     j += 1
-
-    # soma = sum(item[0] * item[1] for item in extra) + sum(lista)
-
-    # print(int(soma))
+    print(int(soma))
