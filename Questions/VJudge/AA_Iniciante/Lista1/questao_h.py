02ea72799ec2b54303926b21c6b6875868ea447d
LauVii <viniciusglaureano@gmail.com>
2024-02-14 13:42:39 -0300

    wrong on test 5

--- Questions/VJudge/AA_Iniciante/Lista1/questao_h.py
+++ Questions/VJudge/AA_Iniciante/Lista1/questao_h.py
@@ -6,8 +6,21 @@ suf = [0] * (n + 1)
 
 for i in range(n):
     pref[i + 1] = lista[i] + pref[i]
+    suf[-i - 2] = lista[i - 1] + suf[-i - 1]
 
-for i in range(n - 1, -1, -1):
-    suf[i] = lista[i] + suf[i + 1]
+result = 0
+ini = 0
+fim = n
 
-print(pref, suf)
+while ini <= fim:
+    if pref[ini] == suf[fim]:
+        result = pref[ini]
+        ini += 1
+        fim -= 1
+
+    while pref[ini] < suf[fim]:
+        ini += 1
+    while pref[ini] > suf[fim]:
+        fim -= 1
+
+print(result)
