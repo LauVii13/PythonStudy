95893909e280a63e54935ee9514ed345ff07d09e
LauVii13 <viniciusglaureano@gmail.com>
2024-02-15 11:55:31 -0300

    tryin to finish

--- Questions/VJudge/AA_Iniciante/Lista1/questao_f.py
+++ Questions/VJudge/AA_Iniciante/Lista1/questao_f.py
@@ -4,7 +4,17 @@ for _ in range(t):
     n, q = map(int, input().split())
     degraus = list(map(int, input().split()))
     degraus.append(0)
+    
+    somatoria = list(map(lambda x: 0, degraus))
+    somatoria[0] = degraus[0]
+
+    for i in range(1, len(degraus)):
+        somatoria[i] = degraus[i] + somatoria[i - 1]
+
+        
     testes = list(map(int, input().split()))
+    med = list(map(lambda x: 0, range(max(testes)+1)))
+    print(med)
     
     som = 0
     
@@ -15,6 +25,7 @@ for _ in range(t):
     
     while p2 < len(testes):
         if testes[p2-1] > testes[p2] and p2 != 0:
+            for 
             p1 = 0
             som = 0
         while degraus[p1] <= testes[p2]:
@@ -26,7 +37,8 @@ for _ in range(t):
                 break
                 
                 
-        outs.append(som)    
+        outs.append(som)
+        med[testes[p2]] = p1
         p2 += 1
         
     for i in outs:
