3707e082626b852e9092e0079a5cf3da8166e219
LauVii <viniciusglaureano@gmail.com>
2024-02-17 16:38:19 -0300

    questao f finished

--- Questions/VJudge/AA_Iniciante/Lista1/questao_f.py
+++ Questions/VJudge/AA_Iniciante/Lista1/questao_f.py
@@ -2,46 +2,28 @@ t = int(input())
 
 for _ in range(t):
     n, q = map(int, input().split())
-    degraus = list(map(int, input().split()))
-    degraus.append(0)
-    
-    somatoria = list(map(lambda x: 0, degraus))
-    somatoria[0] = degraus[0]
+    degrau = list(map(int, input().split()))
+    teste = list(map(int, input().split()))
+    degrau.insert(0, 0)
+    minimo = [degrau[0]]
 
-    for i in range(1, len(degraus)):
-        somatoria[i] = degraus[i] + somatoria[i - 1]
+    for i in range(1, len(degrau)):
+        minimo.append(max(minimo[i - 1], degrau[i]))
+        degrau[i] += degrau[i - 1]
 
-        
-    testes = list(map(int, input().split()))
-    med = list(map(lambda x: 0, range(max(testes)+1)))
-    print(med)
-    
-    som = 0
-    
-    p1 = 0
-    p2 = 0
-    
-    outs = []
-    
-    while p2 < len(testes):
-        if testes[p2-1] > testes[p2] and p2 != 0:
-            for 
-            p1 = 0
-            som = 0
-        while degraus[p1] <= testes[p2]:
-            som += degraus[p1]
-            
-            if p1 < len(degraus)-1:
-                p1+=1
+    for i in range(q):
+        p1 = 0
+        p2 = len(minimo) - 1
+        maior = 0
+        while p1 <= p2:
+            k = (p2 + p1) // 2
+            if minimo[k] <= teste[i]:
+                if minimo[k] > minimo[maior]:
+                    maior = k
+                if minimo[k] == minimo[maior] and k > maior:
+                    maior = k
+                p1 = k + 1
             else:
-                break
-                
-                
-        outs.append(som)
-        med[testes[p2]] = p1
-        p2 += 1
-        
-    for i in outs:
-        print(i, end=" ")
+                p2 = k - 1
+        print(degrau[maior], end=" ")
     print()
-    
