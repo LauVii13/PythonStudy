4bfed3221d9980528f0353f96223e85edf51ea2a
LauVii <viniciusglaureano@gmail.com>
2024-02-22 22:47:42 -0300

    wa

--- Questions/VJudge/AA_Iniciante/Lista2/b_presente_para_gildo.py
+++ Questions/VJudge/AA_Iniciante/Lista2/b_presente_para_gildo.py
@@ -3,11 +3,23 @@ def tira_s(s, t):
     s.pop(0)
 
 
-def tira_t(t, u):
-    u.append(t[-1])
+def tira_t(t):
+    v = t[-1]
     t.pop(-1)
+    return v
 
 
 s = [i for i in input()]
 t = []
-u = []
+u = ""
+
+while len(s) > 0 or len(t) > 0:
+    if len(t) == 0:
+        tira_s(s, t)
+
+    if len(s) == 0 or t[-1] < s[0]:
+        u += tira_t(t)
+    else:
+        tira_s(s, t)
+
+print(u)
