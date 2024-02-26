aa29c279c0b4323a809f3b520be1de61f78d35b0
LauVii <viniciusglaureano@gmail.com>
2024-02-26 17:52:45 -0300

    finished (-5)

--- Questions/VJudge/AA_Iniciante/Lista2/b_presente_para_gildo.py
+++ Questions/VJudge/AA_Iniciante/Lista2/b_presente_para_gildo.py
@@ -1,25 +1,63 @@
-def tira_s(s, t):
-    t.append(s[0])
-    s.pop(0)
+def busca_inicio(positions, p):
+    i, j = 0, len(positions) - 1
+
+    while i < j:
+        v = (i + j) // 2
+        if positions[v] == p:
+            return v
+
+        elif positions[v] > p:
+            j = v
+        else:
+            i = v + 1
+    return i
 
 
-def tira_t(t):
-    v = t[-1]
-    t.pop(-1)
-    return v
-
-
-s = [i for i in input()]
+s = input()
+dictionary = {}
+lista = []
 t = []
 u = ""
 
-while len(s) > 0 or len(t) > 0:
-    if len(t) == 0:
-        tira_s(s, t)
+for i in range(len(s)):
+    lista.append(ord(s[i]) - 97)
+    if dictionary.get(lista[i]) == None:
+        dictionary[lista[i]] = []
 
-    if len(s) == 0 or t[-1] < s[0]:
-        u += tira_t(t)
-    else:
-        tira_s(s, t)
+    dictionary[lista[i]].append(i)
+
+i = 0
+p = 0
+
+while p < len(lista):
+    positions = dictionary.get(i)
+
+    if positions == None:
+        i += 1
+        continue
+
+    while len(t) > 0 and t[-1] <= i:
+        temp = t[-1]
+        t.pop(-1)
+        u += chr(temp + 97)
+
+    if positions[-1] >= p:
+
+        v = busca_inicio(positions, p)
+        for x in range(v, len(positions)):
+            j = positions[x]
+
+            while j > p:
+                t.append(lista[p])
+                p += 1
+
+            u += chr(i + 97)
+            p += 1
+
+    i += 1
+
+while len(t) > 0:
+    u += chr(t[-1] + 97)
+    t.pop(-1)
 
 print(u)
