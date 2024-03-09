24d16266023deeee8060bf3771c245b6af769114
LauVii <viniciusglaureano@gmail.com>
2024-03-08 23:02:22 -0300

    finished(-5)

--- Questions/VJudge/AA_Iniciante/Lista3/c_executivos_introvertidos.py
+++ Questions/VJudge/AA_Iniciante/Lista3/c_executivos_introvertidos.py
@@ -1,38 +1,41 @@
-from queue import PriorityQueue
+import heapq
 
 t = int(input())
 
 for _ in range(t):
     n = int(input())
 
-    pq = PriorityQueue()
+    # Usando uma lista para simular a fila de prioridade
+    pq = []
     lista = list(map(int, input().split()))
 
     for i in range(n):
-        pq.put((-lista[i], i + 1))
+        # Adicionando elementos à lista com o negativo do valor para
+        # simular uma fila de prioridade máxima
+        heapq.heappush(pq, (-lista[i], i + 1))
 
     cont = 0
     resposta = []
 
-    while not pq.empty() and pq.qsize() > 1:
-        num1, id1 = pq.get()
-        num2, id2 = pq.get()
+    while len(pq) > 1:
+        num1, id1 = heapq.heappop(pq)
+        num2, id2 = heapq.heappop(pq)
 
         if num1 == 0 and num2 == 0:
             continue
         if num1 == 0:
-            pq.put((num2, id2))
+            heapq.heappush(pq, (num2, id2))
             continue
         if num2 == 0:
-            pq.put((num1, id1))
+            heapq.heappush(pq, (num1, id1))
             continue
 
         num1 += 1
         num2 += 1
         cont += 1
 
-        pq.put((num1, id1))
-        pq.put((num2, id2))
+        heapq.heappush(pq, (num1, id1))
+        heapq.heappush(pq, (num2, id2))
 
         resposta.append([id1, id2])
 
