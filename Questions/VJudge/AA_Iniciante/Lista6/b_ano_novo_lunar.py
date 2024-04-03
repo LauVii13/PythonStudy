302b82a14d3e62981bcfe488ededac66698803fb
LauVii <viniciusglaureano@gmail.com>
2024-04-02 21:50:38 -0300

    finished (-2)

--- Questions/VJudge/AA_Iniciante/Lista6/b_ano_novo_lunar.py
+++ Questions/VJudge/AA_Iniciante/Lista6/b_ano_novo_lunar.py
@@ -1,32 +1,21 @@
 import heapq
 
 n, m = map(int, input().split())
-nos = {i + 1: set() for i in range(n)}
+nos = {i + 1: [] for i in range(n)}
 
 for _ in range(m):
     a, b = map(int, input().split())
-    nos[a].add(b)
-    nos[b].add(a)
+    nos[a].append(b)
+    nos[b].append(a)
 
-fila = []
+fila = [1]
 heapq.heapify(fila)
-for e in nos[1]:
-    heapq.heappush(fila, e)
-
-visited = list(map(lambda x: False, range(n + 1)))
-visited[1] = True
-
-result = "1 "
-
+visited = {1}
 
 while fila:
     x = heapq.heappop(fila)
-
-    if not visited[x]:
-        visited[x] = True
-        result += f"{x} "
-        for e in nos[x]:
-            if not visited[e]:
-                heapq.heappush(fila, e)
-
-print(result)
+    print(x, end=" ")
+    for e in nos[x]:
+        if not e in visited:
+            heapq.heappush(fila, e)
+            visited.add(e)
