75901e7105fa4bf2497a533332892e5d54e817ec
LauVii <viniciusglaureano@gmail.com>
2024-05-10 10:13:44 -0300

    finished

--- Questions/VJudge/AA_Iniciante/Lista8/c_combinacoes_de_moedas.py
+++ Questions/VJudge/AA_Iniciante/Lista8/c_combinacoes_de_moedas.py
@@ -3,12 +3,12 @@ n_moedas, final = map(int, input().split())
 lista = [0 for _ in range(final + 1)]
 lista[0] = 1
 moedas = list(map(int, input().split()))
-
+moedas.sort()
 for i in range(1, final + 1):
 
     for m in moedas:
         if m > i:
-            continue
+            break
 
         lista[i] = (lista[i] + lista[i - m]) % mod
 
